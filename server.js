d2b7d1b4574237bd84d9a71a4b781132caa0776a
FelixIzm <for_cv@mail.ru>
2020-01-17 16:36:56 +0300

    first

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const fastify = require('fastify')();
+const PORT = process.env.PORT || 3000;
 
 // ROUTES
 fastify.register(require('./routes/users'), {prefix: '/users' });
 
-fastify.listen(3000, function(err, address){
+fastify.listen(PORT, function(err, address){
 	if(err) {
 		console.log(err);
 		process.exit(1);
